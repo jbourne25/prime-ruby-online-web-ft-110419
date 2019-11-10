264cf83a2f83013712ec65eac728c851ef3cfe38
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-10 04:18:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,19 @@
 
-def is_prime(num)
-  n = 2
-  while n < num
-    return false if num % n == 0
-    n += 1
+# def is_prime(num)
+#   n = 2
+#   while n < num
+#     return false if num % n == 0
+#     n += 1
+#   end
+#   true
+# end
+
+def prime(n)
+  unless n.is_a? Integer
+  puts "That's not an integer."
+  if n%2 !=0 && n!=2
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
   end
-  true
 end
